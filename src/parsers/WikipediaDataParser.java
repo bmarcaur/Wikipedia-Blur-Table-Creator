65980f9bb119cb8bf8c3563264cd43b4fac4bfd9
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-07-05 14:02:22 -0400

    started project

---
+++ src/parsers/WikipediaDataParser.java
@@ -0,0 +1,8 @@
+package parsers;
+
+import com.nearinfinity.blur.thrift.generated.Record;
+import com.nearinfinity.blur.thrift.generated.RowMutation;
+
+public class WikipediaDataParser {
+	RowMutation mutation = ne
+}
---
+++ src/processes/WikipediaDataProcess.java
@@ -0,0 +1,9 @@
+package processes;
+
+public class WikipediaDataProcess {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
