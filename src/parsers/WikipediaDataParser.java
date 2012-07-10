4dc8b6174b06cb65eef3db35a350b8abc75ede28
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-07-10 11:18:59 -0400

    moving to map reduce

--- src/parsers/WikipediaDataParser.java
+++ src/parsers/WikipediaDataParser.java
@@ -4,33 +4,43 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.TreeMap;
 
-import models.Revision;
-
 public class WikipediaDataParser {
-	private static String[] namespaces = {"Main", "Talk", "User", "User Talk", "Wikipedia", "Wikipedia Talk"};
-	private static String[] categories = {"None", "Deletion", "Mediation", "Featured Lists", "Featured Pictures", "Arbitration", "Featured Topic", "Featured Portal", "Featured Article", "Featured Sounds", "Good Article"};
-	private static TreeMap<String, LinkedList<String[]>> revisionData = new TreeMap<String, LinkedList<String>>();
 	
-	public static TreeMap<String, LinkedList<String[]>> parseRevisions() {
+	private static String fileLocation = System.getProperty("user.home") + "/Downloads/wikichallenge_data_all/";
+	private static TreeMap<String, ArrayList<ArrayList<String>>> revisionData = new TreeMap<String, ArrayList<ArrayList<String>>>();
+	private static TreeMap<String, String> commentData = new TreeMap<String, String>();
+	private static TreeMap<String, String[]> articleInformation = new TreeMap<String, String[]>();
+	
+	public static TreeMap<String, ArrayList<ArrayList<String>>> parseRevisions() {
 		try {
-			BufferedReader revisionReader = new BufferedReader(new FileReader("training.tsv"));
+			// Call the comment method and add the all the revision comments
+			parseComments();
+			// Open revision file
+			BufferedReader revisionReader = new BufferedReader(new FileReader(fileLocation + "training.tsv"));
 			// Discard the Schema Line
 			revisionReader.readLine();
 			// Buffer for the line in the file
 			String returnedLine;
 			while((returnedLine = revisionReader.readLine()) != null){
 				// Grab the first "column" which is the user id
-				String[] parsedRevisionInfo = returnedLine.split("\\t");
-				String userId = parsedRevisionInfo[0];
+				ArrayList<String> parsedRevisionInfo = new ArrayList<String>(13);
+				parsedRevisionInfo.addAll(Arrays.asList(returnedLine.split("\\t")));
+				// Grab the comment from the comment data structure
+				parsedRevisionInfo.add(commentData.get(parsedRevisionInfo.get(2)));
+				// Free up memory after the comment was used
+				commentData.remove(parsedRevisionInfo.get(2));
+				
+				// Grab the user id for the key
+				String userId = parsedRevisionInfo.get(0);
 				// Add the revision object to the map
 				if (revisionData.containsKey(userId)){
 					revisionData.get(userId).add(parsedRevisionInfo);
 				} else {
-					LinkedList<String[]> newRevisionList = new LinkedList<String[]>();
+					ArrayList<ArrayList<String>> newRevisionList = new ArrayList<ArrayList<String>>(10);
 					newRevisionList.add(parsedRevisionInfo);
 					revisionData.put(userId, newRevisionList);
 				}
@@ -45,13 +55,37 @@ public class WikipediaDataParser {
 			e.printStackTrace();
 			System.exit(1);
 		}
+		
+		// Return a reference to the map we just created
+		return revisionData;
 	}
-
-	public static TreeMap<String, ArrayList<String>> parseComments() {
-
-	}
-
+	
 	public static TreeMap<String, ArrayList<String>> parseArticleInformation() {
+		return null;
+	}
 
+	private static void parseComments() {
+		try {
+			BufferedReader commentReader = new BufferedReader(new FileReader(fileLocation + "comments.tsv"));
+			// Discard the Schema Line
+			commentReader.readLine();
+			// Buffer for the line in the file
+			int countedLines = 0;
+			String returnedLine;
+			while((returnedLine = commentReader.readLine()) != null && countedLines < ){
+				String[] parsedCommentInfo = returnedLine.split("\\t");
+				commentData.put(parsedCommentInfo[0], parsedCommentInfo[1]);	
+				System.out.println(i++);
+			}
+			commentReader.close();
+		} catch (FileNotFoundException e) {
+			System.err.print("File was not found");
+			e.printStackTrace();
+			System.exit(1);
+		} catch (IOException e){
+			System.err.print("A problem occurred while trying to read from the buffered stream.");
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 }
--- src/processes/WikipediaDataProcess.java
+++ src/processes/WikipediaDataProcess.java
@@ -9,10 +9,13 @@ import parsers.*;
 import loaders.*;
 
 public class WikipediaDataProcess {	
+	private static String[] namespaces = {"Main", "Talk", "User", "User Talk", "Wikipedia", "Wikipedia Talk"};
+	private static String[] categories = {"None", "Deletion", "Mediation", "Featured Lists", "Featured Pictures", "Arbitration", "Featured Topic", "Featured Portal", "Featured Article", "Featured Sounds", "Good Article"};
+	
 	public static void main(String[] args) {
 		// Load in all of the files using buffered input streams
-		TreeMap<String, ArrayList<String>> revisions = WikipediaDataParser.parseRevisions();
-		TreeMap<String, ArrayList<String>> comments = WikipediaDataParser.parseComments();
-		TreeMap<String, ArrayList<String>> articleInformation = WikipediaDataParser.parseArticleInformation();
+		TreeMap<String, ArrayList<ArrayList<String>>> revisions = WikipediaDataParser.parseRevisions();
+		// TreeMap<String, String[]> comments = WikipediaDataParser.parseComments();
+		// TreeMap<String, String[]> articleInformation = WikipediaDataParser.parseArticleInformation();
 	}
 }
