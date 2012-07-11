99ae485866a1a84a408ac1aae98993419d6897b8
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-07-11 10:04:46 -0700

    wrote my map reduce job

---
+++ src/processes/WikipediaHadoopDataProcess.java
@@ -0,0 +1,69 @@
+package processes;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapred.lib.MultipleInputs;
+
+@SuppressWarnings("deprecation")
+public class WikipediaHadoopDataProcess {
+	public static void main(String[] args) throws IOException {
+		JobConf configuration = new JobConf(WikipediaHadoopDataProcess.class);
+		configuration.setJobName("Wikipedia Data");
+		
+		configuration.setOutputKeyClass(IntWritable.class);
+		configuration.setOutputValueClass(Text.class);
+		
+		configuration.setCombinerClass(Reduce.class);
+		configuration.setReducerClass(Reduce.class);
+		
+		MultipleInputs.addInputPath(configuration, new Path("/usr/localadmin/wikidata/training.tsv"), TextInputFormat.class, RevisionMapper.class);
+		MultipleInputs.addInputPath(configuration, new Path("/usr/localadmin/wikidata/comments.tsv"), TextInputFormat.class, CommentMapper.class);
+		
+		configuration.setOutputFormat(TextOutputFormat.class);
+		FileOutputFormat.setOutputPath(configuration, new Path("/usr/localadmin/wikidata/output/revisions/"));
+		
+		JobClient.runJob(configuration);
+	}
+	
+	public static class RevisionMapper extends MapReduceBase  implements Mapper<IntWritable, Text, IntWritable, Text> {
+		private IntWritable revisionId = new IntWritable();
+		private Text combinedColumns = new Text();
+		
+		public void map(IntWritable key, Text value, OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
+			String columns = value.toString();
+			String rawRevisionId = columns.split("\\t")[2];
+			revisionId.set(Integer.parseInt(rawRevisionId));
+			combinedColumns.set(columns);
+			output.collect(revisionId, combinedColumns);
+		}
+	}
+	
+	public static class CommentMapper extends MapReduceBase  implements Mapper<IntWritable, Text, IntWritable, Text> {
+		private IntWritable revisionId = new IntWritable();
+		private Text combinedColumns = new Text();
+		
+		public void map(IntWritable key, Text value, OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
+			String columns = value.toString();
+			String rawRevisionId = columns.split("\\t")[0];
+			revisionId.set(Integer.parseInt(rawRevisionId));
+			combinedColumns.set(columns);
+			output.collect(revisionId, combinedColumns);
+		}
+	}
+		
+	public static class Reduce extends MapReduceBase implements Reducer<IntWritable, Text, IntWritable, Text> {
+		public void reduce(IntWritable key, Iterator<Text> values, OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
+			String fullCombinedColumns = new String();
+			while(values.hasNext()){
+				fullCombinedColumns += values.next().toString() + '\t';
+			}
+			output.collect(key, new Text(fullCombinedColumns.trim()));
+		}
+	}
+}
